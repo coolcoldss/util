5dd63fce0245236fdbfc3eea29958daca2febd50
CoolColdUK <coolcold@gmail.com>
2024-03-21 22:00:14 +0000

    upgrade filler array allow generation

--- packages/util/src/array/fillerArray.ts
+++ packages/util/src/array/fillerArray.ts
@@ -1,7 +1,12 @@
 /**
- *
+ * create an array of given length with given value, or generate one along the way
  * @returns array of given length with given value
  */
-export function fillerArray<T>(padValue: T, requiredSize: number): T[] {
-  return new Array(requiredSize).fill(padValue);
+export function fillerArray<T>(padValue: T | ((index: number) => T), requiredSize: number): T[] {
+  const rtn = new Array(requiredSize).fill(padValue);
+
+  if (typeof padValue === 'function') {
+    return rtn.map((fn: (i: number) => T, i) => fn(i));
+  }
+  return rtn;
 }
